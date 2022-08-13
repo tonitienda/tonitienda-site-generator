290f3794306c997bc705d572357a0c61dc00e442
Toni Tienda <tonitienda@gmail.com>
2022-08-13 20:31:06 +0900

    use tutorial that use similar approach but works

---
+++ nextjs-mdx-blog/components/Nav.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+const Nav = () => {
+  return (
+    <nav className="nav p-3 border-bottom">
+      <Link href="/" passHref>
+        <h2 className="pointer">Ebenezer Don</h2>
+      </Link>
+      <Link href="/bio" passHref>
+        <p className="ms-5 pointer lead my-auto">Bio</p>
+      </Link>
+    </nav>
+  );
+};
+export default Nav;
---
+++ nextjs-mdx-blog/pages/_app.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Nav from "../components/Nav";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Next and MDX Blog</title>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <div className="container">
+        <Nav />
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default MyApp;
