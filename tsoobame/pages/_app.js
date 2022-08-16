fabe0861c4dd4d88cd079360e988be18db5c6490
Toni Tienda <tonitienda@gmail.com>
2022-08-16 18:05:53 +0900

    use new way to render mdx

--- tsoobame/pages/_app.js
+++ tsoobame/pages/_app.js
@@ -12,6 +12,9 @@ function MyApp({ Component, pageProps }) {
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
           crossOrigin="anonymous"
         />
+
+        <link rel="stylesheet" href="/css/prism.okaidia.css" />
+        <script src="/scripts/prism.js" />
       </Head>
       <div className="container">
         <Nav />
