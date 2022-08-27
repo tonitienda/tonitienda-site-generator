a199525a5b55e435534af2e56ab1de6acf51923d
Toni Tienda <tonitienda@gmail.com>
2022-08-27 17:22:38 +0900

    fix styles in prod

--- tsoobame/pages/_app.js
+++ tsoobame/pages/_app.js
@@ -4,33 +4,23 @@ import Footer from "../components/Footer";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../components/theme";
 import { Paper } from "@mui/material";
-import createEmotionCache from "../src/createEmotionCache";
-import { CacheProvider } from "@emotion/react";
 
-const clientSideEmotionCache = createEmotionCache();
-
-function MyApp({
-  Component,
-  pageProps,
-  emotionCache = clientSideEmotionCache,
-}) {
+function MyApp({ Component, pageProps }) {
   return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={theme}>
-        <Header />
-        <Paper
-          style={{
-            padding: 0,
-            paddingTop: 100,
-            width: "100%",
-            zIndex: -1,
-          }}
-        >
-          <Component {...pageProps} />
-        </Paper>
-        <Footer />
-      </ThemeProvider>
-    </CacheProvider>
+    <ThemeProvider theme={theme}>
+      <Header />
+      <Paper
+        style={{
+          padding: 0,
+          paddingTop: 100,
+          width: "100%",
+          zIndex: -1,
+        }}
+      >
+        <Component {...pageProps} />
+      </Paper>
+      <Footer />
+    </ThemeProvider>
   );
 }
 
