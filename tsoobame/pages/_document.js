acb23609358c1feb634e705ca924b2b56c543600
Toni Tienda <tonitienda@gmail.com>
2022-08-27 16:21:40 +0900

    Fix

--- tsoobame/pages/_document.js
+++ tsoobame/pages/_document.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
 import theme from "../components/theme";
-import createEmotionCache from "./create-emotion-cache";
+import createEmotionCache from "../src/create-emotion-cache";
 
 export default class MyDocument extends Document {
   render() {
