8afb7bbb09064daca7d82768916a965683c8e1c2
Toni Tienda <tonitienda@gmail.com>
2022-08-27 17:41:05 +0900

    add linkedin badge in about

--- tsoobame/pages/about.jsx
+++ tsoobame/pages/about.jsx
@@ -2,11 +2,30 @@ import { Grid, Typography } from "@mui/material";
 
 const About = () => (
   <Grid container alignItems="space-between" alignContent="space-between">
-    <Grid item md={6}>
-      <Typography variant="h4">Services</Typography>
+    <script
+      src="https://platform.linkedin.com/badges/js/profile.js"
+      async
+      defer
+      type="text/javascript"
+    ></script>
+    <Grid item md={4}>
+      <Typography variant="h5">LinkedIn</Typography>
+
+      <div
+        class="badge-base LI-profile-badge"
+        data-locale="en_US"
+        data-size="medium"
+        data-theme="light"
+        data-type="VERTICAL"
+        data-vanity="tonitienda"
+        data-version="v1"
+      />
     </Grid>
-    <Grid item md={6}>
-      <Typography variant="h4">Links</Typography>
+    <Grid item md={4}>
+      <Typography variant="h5">Services</Typography>
+    </Grid>
+    <Grid item md={4}>
+      <Typography variant="h5">Links</Typography>
     </Grid>
   </Grid>
 );
