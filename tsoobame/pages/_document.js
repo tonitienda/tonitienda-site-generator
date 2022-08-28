7b666c41ba345b94f73bd0f9ea73c5419a953f46
Toni Tienda <tonitienda@gmail.com>
2022-08-28 11:31:28 +0900

    add google analytics

--- tsoobame/pages/_document.js
+++ tsoobame/pages/_document.js
@@ -6,7 +6,26 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>{this.props.emotionStyleTags}</Head>
+        <Head>
+          {this.props.emotionStyleTags}
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-Z67DVHFVCM`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Z67DVHFVCM', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
