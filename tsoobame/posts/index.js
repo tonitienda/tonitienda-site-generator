0459532301f2ca6e61626088ea060039850ad771
Toni Tienda <tonitienda@gmail.com>
2023-05-23 19:04:37 +0900

    post: function-call-cascading

--- tsoobame/posts/index.js
+++ tsoobame/posts/index.js
@@ -2,12 +2,14 @@ import dataLoader from "./data-loader.json";
 import graphqlSchema from "./graphql-schema-stitching.json";
 import shapeVsOptionality from "./shape-vs-optionality.json";
 import dockerRegistryMirror from "./docker-registry-mirror.json";
+import functionCallCascading from "./function-call-cascading.json";
 
 export default [
   dataLoader,
   graphqlSchema,
   shapeVsOptionality,
   dockerRegistryMirror,
+  functionCallCascading,
 ].map((post) => ({
   ...post,
   slug: post.filePath.split(".")[0],
